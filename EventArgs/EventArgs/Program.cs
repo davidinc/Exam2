46b082a8dd84367d28b9cb30f27b396ef7152c76
Dawit Ayalew <davidintsum@gmail.com>
2023-12-11 20:43:57 -0500

    EventArgs
    Write classes for events, one class which exposes an event and another which handles the event.
    
    Choose any scenario you prefer to build your code.
    
    1)   Create a class to pass as an argument for the event handlers (EventArgs class)
    
    2)   Set up the delegate for the event
    
    3)   Declare the code for the event
    
    4)   Create code the will be run when the event occurs (Event Handler)
    
    5)   Associate the event with the event handler

---
+++ EventArgs/EventArgs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// Step 1: Create a class to pass as an argument 
+public class MyEventArgs : EventArgs
+{
+    public string Message { get; }
+
+    public MyEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+public delegate void MyEventHandler(object sender, MyEventArgs args);
+
+public class EventPublisher
+{
+    public event MyEventHandler MyEvent;
+
+    protected virtual void OnMyEvent(string message)
+    {
+        MyEventHandler handler = MyEvent;
+        handler?.Invoke(this, new MyEventArgs(message));
+    }
+
+    public void DoSomething()
+    {
+        OnMyEvent("Event occurred!");
+    }
+}
+
+public class EventSubscriber
+{
+    public EventSubscriber(EventPublisher publisher)
+    {
+
+        publisher.MyEvent += HandleEvent;
+    }
+
+    private void HandleEvent(object sender, MyEventArgs args)
+    {
+        Console.WriteLine("Event handled. Message: " + args.Message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EventPublisher publisher = new EventPublisher();
+        EventSubscriber subscriber = new EventSubscriber(publisher);
+
+       publisher.DoSomething();
+    }
+}
